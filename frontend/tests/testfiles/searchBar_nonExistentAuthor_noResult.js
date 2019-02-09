336e63f221c88472765f738d19bc6abb60d2c66e
Yong Hao TENG <yonghao.teng@gmail.com>
2019-02-09 21:41:25 +0800

    Fix files

--- frontend/tests/testfiles/searchBar_nonExistentAuthor_noResult.js
+++ frontend/tests/testfiles/searchBar_nonExistentAuthor_noResult.js
@@ -3,9 +3,9 @@ describe('search non-existent author', function () {
     const page = await browser.newPage();
     await page.goto('http://localhost:9000', {timeout: 0, waitUntil: 'domcontentloaded'});
     await page.type('div#summary input', 'AAA%AAsAABAA$AAnAACAA-AA(AADAA;AA)');
-    await page.keyboard.press('Enter'); 
+    await page.keyboard.press('Enter');
     const results = await page.evaluate(() => document.querySelector('#summary-charts').innerHTML);
-    
+
     expect(results).to.be.empty;
   });
 });
--- frontend/tests/testfiles/searchBar_uniqueAuthor_oneResult.js
+++ frontend/tests/testfiles/searchBar_uniqueAuthor_oneResult.js
@@ -3,9 +3,9 @@ describe('search unique author', function () {
     const page = await browser.newPage();
     await page.goto('http://localhost:9000', {timeout: 0, waitUntil: 'domcontentloaded'});
     await page.type('div#summary input', 'Yong Hao TENG');
-    await page.keyboard.press('Enter'); 
+    await page.keyboard.press('Enter');
     const results = await page.evaluate(() => document.querySelector('#summary-charts').children.length);
-    
+
     expect(results).to.be.equal(1);
   });
 });
